e5ec36201b773629fcd62f42faa6845b00f6a862
Thitaree Porameesanaporn <154073919+Engvatcharb@users.noreply.github.com>
2024-02-04 22:05:04 +0700

    Update CallAPI.py

--- Project10_Python/CallAPI.py
+++ Project10_Python/CallAPI.py
@@ -1,1 +1,20 @@
+## import module
+from requests import get
+from time import sleep
 
+url = "https://swapi.dev/api/people/"
+
+response = get(url)
+## 200 = OK
+print (response.status_code)
+
+## see data that get back from server
+for i in range(10):
+ index = i + 1
+ new_url = url + str(index)
+ resp = get(new_url).json()
+ name = resp["name"]
+ height = str(resp["height"])
+ mass = str(resp["mass"])
+ print (f"Character {index} in starwar name : {name} height : {height} mass : {mass}")
+ sleep(2)
